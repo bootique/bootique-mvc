b0be8b46a9177d635313a4012655f70dc97bd5fb
Andrus Adamchik <andrus@objectstyle.com>
2026-03-05 16:47:24 -0500

    upgrading to module-parents 4.0.5

--- bootique-mvc-mustache/src/test/java/io/bootique/mvc/mustache/TestWithTemplateServer.java
+++ bootique-mvc-mustache/src/test/java/io/bootique/mvc/mustache/TestWithTemplateServer.java
@@ -24,10 +24,10 @@ import io.bootique.BQRuntime;
 import io.bootique.Bootique;
 import io.bootique.jetty.JettyModule;
 import io.bootique.jetty.MappedServlet;
-import io.bootique.jetty.junit5.JettyTester;
-import io.bootique.junit5.BQApp;
-import io.bootique.junit5.BQTest;
-import io.bootique.junit5.BQTestScope;
+import io.bootique.jetty.junit.JettyTester;
+import io.bootique.junit.BQApp;
+import io.bootique.junit.BQTest;
+import io.bootique.junit.BQTestScope;
 
 
 @BQTest
